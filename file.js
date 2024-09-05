df392029577744cd6b61390652dd15362338151f
shawn835 <nahashonm851@gmail.com>
2024-09-05 03:27:30 +0300

    removed some codes

--- file.js
+++
@@ -1,7 +0,0 @@
-const humbergur = document.getElementById('closed')
-
-const paragraph = document.querySelectorAll('div')
-console.log(paragraph)
-humbergur.addEventListener('click', () => {
-    humbergur.classList.toggle('open')
-})
